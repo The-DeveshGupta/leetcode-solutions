53d3d4eb3c8aeb09866e0d4334ba65ac9fd76719
The-DeveshGupta <devesh1997.gupta@gmail.com>
2023-03-18 13:27:15 +0530

    1472.py added

---
+++ 1401-1500/1472.py
@@ -0,0 +1,28 @@
+len(self.page_list) - 1class BrowserHistory:
+
+    def __init__(self, homepage: str):
+        self.page_list = [homepage]
+        self.pointer = 0
+
+
+    def visit(self, url: str) -> None:
+        self.page_list = self.page_list[:self.pointer+1]
+        self.page_list.append(url)
+        self.pointer += 1
+
+
+    def back(self, steps: int) -> str:
+        self.pointer = 0 if self.pointer < steps else self.pointer - steps
+        return self.page_list[self.pointer]
+
+
+    def forward(self, steps: int) -> str:
+        self.pointer = len(self.page_list) - 1 if len(self.page_list) - 1 < self.pointer + steps else self.pointer + steps
+        return self.page_list[self.pointer]
+
+
+# Your BrowserHistory object will be instantiated and called as such:
+# obj = BrowserHistory(homepage)
+# obj.visit(url)
+# param_2 = obj.back(steps)
+# param_3 = obj.forward(steps)
